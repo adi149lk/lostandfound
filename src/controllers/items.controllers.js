1dba7b2ad4fd07e8a28f18640a91d6420018c7fb
Aditya <apgbgbf@gmail.com>
2026-04-10 12:02:01 +0530

    add deleteitem api with dynamic routing

--- src/controllers/items.controllers.js
+++ src/controllers/items.controllers.js
@@ -15,9 +15,14 @@ exports.getItems = async (req, res, next) => {
 };
 exports.postItems = async (req, res, next) => {
   const { name, description, status, location, phone } = req.body;
+  if (!name || !description || !location || !phone) {
+    return res.status(400).json({
+      message: "All fields are required",
+    });
+  }
   const isPerson = await personModel.findOne({ phone: phone });
   if (!isPerson) {
-    return res.status(500).json({
+    return res.status(404).json({
       message: "invaild user  1st entry the person info ",
     });
   }
@@ -29,12 +34,32 @@ exports.postItems = async (req, res, next) => {
       location,
       associate: isPerson._id,
     });
-    return res.status(200).json({
+    return res.status(201).json({
       item,
     });
   } catch (err) {
     res.status(500).json({
       message: "err while add item",
+      error: err.message,
+    });
+  }
+};
+exports.deleteItems = async (req, res, next) => {
+  const { itemId } = req.params;
+  try {
+    const item = await itemModel.findOneAndDelete({ _id: itemId });
+    if (!item) {
+      return res.status(404).json({
+        message: "Item not found",
+      });
+    }
+    return res.status(200).json({
+      message: "Item deleted",
+      item,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "err while delete item",
       error: err.message,
     });
   }
--- src/routes/item.routes.js
+++ src/routes/item.routes.js
@@ -1,7 +1,12 @@
 const express = require("express");
-const { getItems, postItems } = require("../controllers/items.controllers");
+const {
+  getItems,
+  postItems,
+  deleteItems,
+} = require("../controllers/items.controllers");
 const itemRo = express.Router();
 const checkLogin = require("../middlewares/auth.middleware");
 itemRo.get("/item", getItems);
 itemRo.post("/item", [checkLogin, postItems]);
+itemRo.delete("/item/:itemId", [checkLogin, deleteItems]);
 module.exports = itemRo;
